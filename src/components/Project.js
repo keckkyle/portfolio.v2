a9c6b0b2024a2d980f8102e4aa7649b714ea838d
Kyle Keck <keckkyle@gmail.com>
2020-08-16 15:51:00 -0400

    Update Projects and Project components

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,45 +1,26 @@
 import React from 'react'
 
-class Project extends React.Component {
-
-    displayRef = React.createRef()
-    titleRef = React.createRef()
-
-    handleClick = () => {
-        const details = this.displayRef.current
-        const title = this.titleRef.current
-        details.classList.toggle('open')
-        title.classList.toggle('open')
-    }
-
-    render(){
-        const { handleClick, displayRef, titleRef } = this;
-        const { project } = this.props
-        const { title, image, technologies, purpose, details, link} = project;
-        return(
-            <div className="Project">
-                <div onClick={handleClick} className="project-link">
-                    <p className="text">{title}</p>
-                    <p className="arrow">➤</p>
-                </div>
-                <div ref={titleRef} className="project-title">
-                    <button  onClick={handleClick}>❮ back</button>
-                    <h4>{title}</h4>
-                </div>
-                <div ref={displayRef} className="details">
-                    <div className="project-brief">
-                        { project['image'] === "https://s3.amazonaws.com/keckkyle.com.images/undefined" ? <></> : <img src={image} alt={title}/>}
-                        <div>
-                            <p>{purpose}</p>
-                            <p>Technologies: {technologies}</p>
-                            { !link ? <></>:<p><a href={link} target="_blank" rel="noopener noreferrer"> {link}</a></p> }
-                        </div>
-                    </div>
-                    <p>{details}</p>
-                </div>
+const Project = (props) => {
+    const { title, image, technologies, purpose, details, link } = props.project;
+    return (
+        <div className="page">
+            <div className="project-title">
+                <button  onClick={props.closeProject}>❮ back</button>
+                <h4>{title}</h4>
             </div>
-        )
-    }
+            <div className="details">
+                <div className="project-brief">
+                    { image === "https://s3.amazonaws.com/keckkyle.com.images/undefined" ? <></> : <img className="project-img" src={image} alt={title}/>}
+                    <div>                          
+                        <p>{purpose}</p>
+                        <p>Technologies: {technologies}</p>
+                        { !link ? <></>:<p><a href={link} target="_blank" rel="noopener noreferrer"> {link}</a></p> }
+                    </div>
+                </div>
+                <p>{details}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Project
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,19 +1,62 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 import Project from '../components/Project'
 import '../css/Projects.css';
 
-const Projects = (props) => (
-    <div className="page">
-        <div className="Projects">
-            <h3>Projects</h3>
-            {props.projects.map(item =>
-                <Project 
-                  key={item.id} 
-                  project={item}
-                />
-            )}                
-        </div>
-    </div>
-)
+class Projects extends React.Component {
+    state = {
+        viewProject: false,
+        projectIndex: -1,
+    }
+
+    setProjectIndex = (index) => {
+        this.setState({
+            projectIndex: index,
+        })
+    }
+
+    setViewProject = () => {
+        this.setState({
+            viewProject: !this.state.viewProject,
+        })
+    }
+
+    openProject = (index) => {
+        this.setProjectIndex(index)
+        this.setViewProject()
+    }
+
+    render(){
+        const { viewProject , projectIndex } = this.state
+        const { projects } = this.props
+        return (
+            <div className="page">
+                <div className="Projects">
+                    <h3>Projects</h3>
+                    <div className="project-list">
+                        {projects.map((item, index) =>
+                            <div key={item.id} onClick={() => this.openProject(index)} className="project-link">
+                                <p className="text">{item.title}</p>
+                                <p className="arrow">➤</p>
+                            </div>
+                        )}   
+                    </div>  
+                    <CSSTransition
+                        in = {viewProject}
+                        timeout = {400}
+                        classNames = 'Projects'
+                        unmountOnExit
+                        onExited = {() => this.setProjectIndex(-1)}
+                    >
+                        <Project 
+                          project={projects[projectIndex]} 
+                          closeProject={this.setViewProject}
+                        />
+                    </CSSTransition>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Projects
