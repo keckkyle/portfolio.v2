5893a6d7b854dd995ad2c1c5890795857b11ec76
Kyle Keck <keckkyle@gmail.com>
2020-08-29 14:26:15 -0400

    Updates to page padding, update to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -40,17 +40,8 @@ const About = (props) => (
                 </div>
                 <div className="about-item">
                     <img src={about[5]} />
-                    <p>While living in Thailand, I began getting back into web development and started studying web development and building personal projects during my free time. In the summer of 2019, I went to Kuala Lumpur, Malaysia to attend <a href="https://www.nextacademy.com/" target="_blank" rel="noopener noreferrer">NEXT Academy</a>'s Full-Stack Web Developer course.</p>
+                    <p>While living in Thailand, I began getting back into web development and started studying web development and building personal projects during my free time. In the summer of 2019, I went to Kuala Lumpur, Malaysia to attend the Full-Stack Web Developer course at <a href="https://www.nextacademy.com/" target="_blank" rel="noopener noreferrer">NEXT Academy</a>.</p>
                 </div>
-                {/* <p>
-                    I grew up in Michigan in a family who wasn’t content to just sit at home. We took vacations to different parts of the world which made me interested in tourism. This interest led me study it at Michigan State University. While attending MSU, I started running the website for MSU’s Modern Greek Club. I enjoyed spending time on the project up until I graduated with my degree in tourism
-                </p>
-                <p>
-                    Since university, I’ve worked in varying industries in different countries. I considered pursuing web design more seriously, but life always seemed to get in the way. While teaching in Thailand, I finally had the opportunity to make that desire a reality. I started learning all that I could through online bootcamps and anything else that would satiate my desire to learn more. I quickly decided that I wanted to be a Web Developer which motivated me to attend a Full-Stack Web Development bootcamp in Kuala Lumpur.
-                </p>
-                <p>
-                    I’m always looking for more opportunities to become a better web developer and am pursuing a position to do so. I want to hone my skills and start this new chapter of my life exclusively as a Web Developer. I’m excited to overcome challenges and produce beautifully crafted pages and elements for others to experience and enjoy.
-                </p> */}
             </section>
         </div>
     </div>
