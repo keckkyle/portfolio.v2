7fae9e5c294cf947c445cf061edb4724c05568ac
Kyle Keck <keckkyle@gmail.com>
2024-08-10 04:35:19 -0800

    add photo

--- src/Components/Bryan.js
+++ src/Components/Bryan.js
@@ -3,6 +3,7 @@ import "../CSS/Bryan.css";
 import Letter from "./Letter";
 import Signature from "./Signature";
 import Opening from "./Opening";
+import Us from "../Image/Us";
 
 function Bryan() {
   const [show, setShow] = React.useState(false);
@@ -12,6 +13,9 @@ function Bryan() {
       <p className={`salutation`}>Dear Bryan</p>
       <Letter isVisible={show} />
       <Opening isHidden={show} handleOnClick={setShow} />
+      <div className="photo">
+        <Us />
+      </div>
       <Signature />
     </div>
   );
